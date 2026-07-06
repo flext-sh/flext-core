64ba13f2b0cf59d399cadb5aef7402ef3a3a0ee8
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-05 22:36:50 -0300

    fixes

--- src/flext_core/_root_typing_parts/__init__.py
+++ src/flext_core/_root_typing_parts/__init__.py
@@ -3,10 +3,27 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from flext_core._root_typing_parts._exports import (
     FLEXT_CORE__ROOT_TYPING_PARTS_LAZY_IMPORTS,
 )
 from flext_core.lazy import install_lazy_exports
+
+if TYPE_CHECKING:
+    from flext_core._root_typing_parts.facades import (
+        c as c,
+        d as d,
+        e as e,
+        h as h,
+        m as m,
+        p as p,
+        r as r,
+        s as s,
+        t as t,
+        u as u,
+        x as x,
+    )
 
 _LAZY_IMPORTS = FLEXT_CORE__ROOT_TYPING_PARTS_LAZY_IMPORTS
 
--- src/flext_core/_utilities/_exports.py
+++ src/flext_core/_utilities/_exports.py
@@ -3,22 +3,106 @@
 
 from __future__ import annotations
 
-from flext_core._utilities._exports_lazy_part_01 import (
-    FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_01,
-)
-from flext_core._utilities._exports_lazy_part_02 import (
-    FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_02,
-)
-from flext_core._utilities._exports_lazy_part_03 import (
-    FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_03,
-)
-from flext_core.lazy import merge_lazy_imports
+from flext_core.lazy import build_lazy_import_map, merge_lazy_imports
 
-_LOCAL_LAZY_IMPORTS = {
-    **FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_01,
-    **FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_02,
-    **FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_03,
-}
+_LOCAL_LAZY_IMPORTS = build_lazy_import_map(
+    {
+        "._beartype": ("_beartype",),
+        "._beartype._alias_visitor": ("FlextUtilitiesBeartypeAliasVisitor",),
+        "._beartype._class_visitor_parts.class_visitor_part_03": (
+            "FlextUtilitiesBeartypeClassVisitor",
+        ),
+        "._beartype._helpers_parts.helpers_part_03": ("FlextUtilitiesBeartypeHelpers",),
+        "._beartype._library_visitor": ("FlextUtilitiesBeartypeLibraryVisitor",),
+        "._beartype.attr_visitor": ("FlextUtilitiesBeartypeAttrVisitor",),
+        "._beartype.deprecated_visitor": ("FlextUtilitiesBeartypeDeprecatedVisitor",),
+        "._beartype.field_visitor": ("FlextUtilitiesBeartypeFieldVisitor",),
+        "._beartype.import_visitor": ("FlextUtilitiesBeartypeImportVisitor",),
+        "._beartype.method_visitor": ("FlextUtilitiesBeartypeMethodVisitor",),
+        "._beartype.module_visitor": ("FlextUtilitiesBeartypeModuleVisitor",),
+        "._checker_parts": ("_checker_parts",),
+        "._checker_parts.checker_part_03": ("FlextUtilitiesChecker",),
+        "._context_crud_set": ("FlextUtilitiesContextCrudSetMixin",),
+        "._enforcement_collect_parts": ("_enforcement_collect_parts",),
+        "._enforcement_collect_parts.enforcement_collect_part_02": (
+            "FlextUtilitiesEnforcementCollect",
+        ),
+        "._enforcement_parts": ("_enforcement_parts",),
+        "._enforcement_parts.enforcement_part_01": ("PREDICATE_BINDINGS",),
+        "._enforcement_parts.enforcement_part_05": ("FlextUtilitiesEnforcement",),
+        "._enforcement_parts.enforcement_part_06": ("EXTENDED_PREDICATE_BINDINGS",),
+        "._exports": ("FLEXT_CORE__UTILITIES_LAZY_IMPORTS",),
+        "._exports_lazy_part_01": ("FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_01",),
+        "._exports_lazy_part_02": ("FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_02",),
+        "._exports_lazy_part_03": ("FLEXT_CORE__UTILITIES_LAZY_IMPORTS_PART_03",),
+        "._generators_parts": ("_generators_parts",),
+        "._generators_parts.generators_part_02": ("FlextUtilitiesGenerators",),
+        "._guards_parts": ("_guards_parts",),
+        "._guards_parts.guards_part_02": ("FlextUtilitiesGuards",),
+        "._guards_type_protocol_specs": ("FlextUtilitiesGuardsTypeProtocolSpecsMixin",),
+        "._guards_type_protocol_string": (
+            "FlextUtilitiesGuardsTypeProtocolStringMixin",
+        ),
+        "._guards_type_protocol_types": ("ProtocolGuardInput",),
+        "._logging_config_parts": ("_logging_config_parts",),
+        "._logging_config_parts.logging_config_part_03": (
+            "FlextUtilitiesLoggingConfig",
+        ),
+        "._logging_context_parts": ("_logging_context_parts",),
+        "._logging_context_parts.logging_context_part_02": (
+            "FlextUtilitiesLoggingContext",
+        ),
+        "._mapper_access_parts": ("_mapper_access_parts",),
+        "._mapper_access_parts.mapper_access_part_02": ("FlextUtilitiesMapperAccess",),
+        "._mapper_extract_parts": ("_mapper_extract_parts",),
+        "._mapper_extract_parts.mapper_extract_part_02": (
+            "FlextUtilitiesMapperExtract",
+        ),
+        "._parser_targets_parts": ("_parser_targets_parts",),
+        "._parser_targets_parts.parser_targets_part_02": (
+            "FlextUtilitiesParserTargets",
+        ),
+        "._project_metadata_parts": ("_project_metadata_parts",),
+        "._project_metadata_parts.project_metadata_part_04": (
+            "FlextUtilitiesProjectMetadata",
+        ),
+        ".args": ("FlextUtilitiesArgs",),
+        ".beartype_conf": ("FlextUtilitiesBeartypeConf",),
+        ".beartype_engine": (
+            "FlextUtilitiesBeartypeEngine",
+            "ube",
+        ),
+        ".beartype_typingext_patch": ("FlextUtilitiesBeartypeTypingExtPatch",),
+        ".collection": ("FlextUtilitiesCollection",),
+        ".collection_iter": ("FlextUtilitiesCollectionIter",),
+        ".collection_merge": ("FlextUtilitiesCollectionMerge",),
+        ".context": ("FlextUtilitiesContext",),
+        ".context_crud": ("FlextUtilitiesContextCrud",),
+        ".context_lifecycle": ("FlextUtilitiesContextLifecycle",),
+        ".context_state": ("FlextUtilitiesContextState",),
+        ".conversion": ("FlextUtilitiesConversion",),
+        ".discovery": ("FlextUtilitiesDiscovery",),
+        ".dispatcher_execute": ("execute_dispatcher_handler",),
+        ".domain": ("FlextUtilitiesDomain",),
+        ".enforcement_emit": ("FlextUtilitiesEnforcementEmit",),
+        ".enum": ("FlextUtilitiesEnum",),
+        ".guards_type_core": ("FlextUtilitiesGuardsTypeCore",),
+        ".guards_type_model": ("FlextUtilitiesGuardsTypeModel",),
+        ".guards_type_protocol": ("FlextUtilitiesGuardsTypeProtocol",),
+        ".handler": ("FlextUtilitiesHandler",),
+        ".mapper": ("FlextUtilitiesMapper",),
+        ".model": ("FlextUtilitiesModel",),
+        ".model_options": ("FlextUtilitiesModelOptions",),
+        ".model_runtime": ("FlextUtilitiesModelRuntime",),
+        ".parser": ("FlextUtilitiesParser",),
+        ".parser_coerce": ("FlextUtilitiesParserCoerce",),
+        ".pydantic": ("FlextUtilitiesPydantic",),
+        ".reliability": ("FlextUtilitiesReliability",),
+        ".runtime_violation_registry": ("FlextUtilitiesRuntimeViolationRegistry",),
+        ".settings": ("FlextUtilitiesSettings",),
+        ".text": ("FlextUtilitiesText",),
+    },
+)
 
 FLEXT_CORE__UTILITIES_LAZY_IMPORTS = merge_lazy_imports(
     (
